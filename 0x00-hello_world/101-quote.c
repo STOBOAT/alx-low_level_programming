d1a7a58e777451c698cf7865b3ff3c9f5030be99
STOBOAT <stoboat2015@gmail.com>
2023-06-19 02:19:15 -0700

    prints the last digit of the number stored in the variable

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - entry
+*description - Prints "\"Programming is like building a multilingual puzzle"
+*Return: 1 Always(succes)
+*/
+int main(void)
+{
+	fwrite(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
