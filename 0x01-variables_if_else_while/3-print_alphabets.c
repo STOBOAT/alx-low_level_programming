4b69f90f4f7be6211ba6fd8a2d1c19e499957158
STOBOAT <stoboat2015@gmail.com>
2023-06-19 06:12:34 -0700

    a program that prints the alphabet in lowercase and UPPERCASE

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -14,11 +14,11 @@ int main(void)
 		putchar(a);
 		a = a + 1;
 	}
-	putchar('\n');
-	while (A <= 98)
+	while (A <= 90)
 	{
 		putchar(A);
 		A = A + 1;
 	}
-		return (0);
+	putchar('\n');
+	return (0);
 }
