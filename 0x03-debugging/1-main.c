7c7941559b269dbbba44e0b12c47a50bb333c4d1
STOBOAT <stoboat2015@gmail.com>
2023-06-22 08:42:58 -0700

    This program prints the largest of three integers

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,9 +10,9 @@ int main(void)
 
 	printf("Infinite loop incoming :(\n");
 	i = 0;
-	/*while (i < 10)*/
+	/*while (i < 10) this statement is always true */
 	/*{*/
-	/*putchar(i);*/
+	/*putchar(i);this character is printed indefinitely*/
 	/*}*/
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * largest_number - returns the largest of 3 numbers
+ *
+ * @a: the first integer
+ *
+ * @b: the second integer
+ *
+ * @c: the third integer
+ *
+ * Return: the largest number
+*/
+int largest_number(int a, int b, int c)
+{
+	int largest;
+
+	if (a > b && a > c)
+	{
+		largest = a;
+	}
+	else if (a > b && c > a)
+	{
+		largest = c;
+	}
+	else if (b > c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+	return (largest);
+}
+
