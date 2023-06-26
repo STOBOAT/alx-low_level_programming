a12407b0d098501eb4d6cc920cef8bb47a00813b
STOBOAT <stoboat2015@gmail.com>
2023-06-26 16:55:08 -0700

    a function that copies the string pointed to by src

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array of integers.
+ * @a: input array.
+ * @n: input n elements
+ * Return: no return.
+ */
+void print_array(int *a, int n)
+{
+	int m;
+
+	for (m = 0; m < n; m++)
+	{
+		printf("%d", a[m]);
+		if (m != (n - 1))
+			printf(", ");
+	}
+	printf("\n");
+}
