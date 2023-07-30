258f83d02a1e83da30fadcc3587fca21cfb8c3c9
STOBOAT <stoboat2015@gmail.com>
2023-07-29 17:04:42 -0700

    a function that adds a new node at the end of a list_t list.

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,16 @@
 #ifndef LISTS_H
 #define LISTS_H
- 
-typedef struct list_s {
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
     char *str;
     unsigned int len;
     struct list_s *next;
