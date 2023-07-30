6987c0d08c37f3897e02e307631111df7b764260
STOBOAT <stoboat2015@gmail.com>
2023-07-29 19:10:13 -0700

    a function that prints You're beat

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,14 @@
-#include < stdio.h>
+#include <stdio.h>
 
-
-void first(void) __attribute__ ((constructor));
+/**
+ * first - Constructor function that is automatically executed before main.
+ *
+ * This function will be called automatically before the main function
+ * is executed due to the __attribute__((constructor)) attribute.
+ */
+void first(void) __attribute__((constructor));
+void first(void)
 {
-	void first(void) 
-	{
-		printf("You're beat! and yet, you must allow,\n");
-		printf("I bore my house upon my back!\n");
-	}
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
