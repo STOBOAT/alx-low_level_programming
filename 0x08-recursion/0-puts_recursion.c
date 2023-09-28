ac30c10cb6c873908d0768b35d752bacac6384b7
STOBOAT <stoboat2015@gmail.com>
2023-09-27 21:20:14 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * _puts_recursion - function to print a string
+ *
+ *@s: string to be printed
+ *
+ * Return: Always 0
+ */
+void _puts_recursion(char *s);
+
+void _puts_recursion(char *s)
+{
+    if (*s == '\0')
+    {
+        return;
+    }
+    else
+    {
+        _putchar(*s);
+        _puts_recursion(s + 1);
+    }
+}
+/**
+* main - Entry point of the code
+*
+* Return: Always 0 (Success)
+*/
+int main()
+{
+    _puts_recursion("Puts with recursion");
+    _putchar('\n');
+    
+    return 0;
+}
