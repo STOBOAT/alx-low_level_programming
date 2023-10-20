44528c7a0716d27184ec60664ea948cc2e11f657
STOBOAT <stoboat2015@gmail.com>
2023-10-19 22:21:03 -0700

    zero puts recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,22 +10,22 @@ void _puts_recursion(char *s);
 
 void _puts_recursion(char *s)
 {
-    if (*s == '\0')
-    {
-        return;
-    }
-    else
-    {
-        _putchar(*s);
-        _puts_recursion(s + 1);
-    }
+	if (*s == '\0')
+	{
+		return;
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 }
 /**
 * main - Entry point of the code
 *
 * Return: Always 0 (Success)
 */
-int main()
+int main(void)
 {
     _puts_recursion("Puts with recursion");
     _putchar('\n');
