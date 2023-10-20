dfdbaa8522dba096225cb83b7345e465ccd199d9
STOBOAT <stoboat2015@gmail.com>
2023-10-19 22:43:35 -0700

    update

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,19 @@
-#include <stdio.h>
-
-void first(void) __attribute__ ((constructor));
+#include "lists.h"
 
 /**
- * first - prints a sentence before the main
- * function is executed
+ * free_list - frees a list
+ * @head: head of the linked list.
+ * Return: no return.
  */
-void first(void)
+
+void free_list(list_t *head)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	list_t *current;
+
+	while ((current = head) != NULL)
+	{
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
 }
