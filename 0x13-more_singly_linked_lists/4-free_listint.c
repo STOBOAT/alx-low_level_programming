8ee41b5dd448f845e711996b847c8c1839d0ced0
STOBOAT <stoboat2015@gmail.com>
2023-10-24 10:37:10 -0700

    function that frees a listint_t list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,5 +1,6 @@
-82% of storage used … If you run out, you won't have enough storage to create, edit and upload files. Get 100 GB of storage for GH₵9.00 GH₵2.25/month for 3 months.
-4-free_listint.c
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
